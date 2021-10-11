60e16b21d70f6f65dc6eb4da216d9da9d6d31de0
jeffrey-cabreza <cabrezaj@gmatc.matc.edu>
2021-10-11 12:30:37 -0500

    Activity 3 Updated

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Value> Values {get; set;}
+    
+
+    public DataContext(DbContextOptions options) : base(options)
+        {
+        
+        }
+    }
+}
